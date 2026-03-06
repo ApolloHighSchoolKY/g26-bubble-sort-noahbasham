f0b5a147238593f304c9b91b538102eb3263436a
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-03-06 04:48:40 +0000

    commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -9,15 +9,26 @@ public class BubbleSort
 		int tempNum;
 
 		//Loop once for each pass, where passes is one less than the number of items.
+		for(passes = 1; passes<myNums.length; passes++)
+		{
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
+			for(comparisons = 0; comparisons < myNums.length-passes; comparisons++)
+			{
+				tempNum = myNums[comparisons + 1];
 				//If they are out of order, swap the values
+				if(myNums[comparisons] >= tempNum)
+				{
+					myNums[comparisons + 1] = myNums[comparisons];
+					myNums[comparisons] = tempNum;
+				}
 			//End Loop for comparisons
-    
+			}
 			//The next pass will use one less comparison
     
 		//End Loop for passes
-
-
+		}
+		for(int x: myNums)
+			System.out.println(x);
 
   }
 }
